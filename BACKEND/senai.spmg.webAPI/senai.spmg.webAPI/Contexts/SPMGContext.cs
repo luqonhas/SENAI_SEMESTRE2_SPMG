6a43675cf73774282817cd41bbbe4e963d734f1c
luqonhas <lucas.apolinario98@gmail.com>
2021-08-26 10:06:27 -0300

    Erro da pasta FotosBackUp corrigido!

--- BACKEND/senai.spmg.webAPI/senai.spmg.webAPI/Contexts/SPMGContext.cs
+++ BACKEND/senai.spmg.webAPI/senai.spmg.webAPI/Contexts/SPMGContext.cs
@@ -34,7 +34,7 @@ namespace senai.spmg.webAPI.Contexts
             if (!optionsBuilder.IsConfigured)
             {
                 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-HMTUR0P; initial catalog=SPMG_DBFIRST; user Id=SA; pwd=Soufoda2;");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CRF35C2\\SQLEXPRESS; initial catalog=SPMG_DBFIRST; Integrated Security=true");
             }
         }
 
